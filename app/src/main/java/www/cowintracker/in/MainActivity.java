afa185ab23f2817fd5b11a8673b46b777eed5b60
masud-ahmed-alt <ma7332125@gmail.com>
2021-06-10 21:07:30 +0530

    bug fixes and add splash screen and app icon

--- app/src/main/java/www/cowintracker/in/MainActivity.java
+++ app/src/main/java/www/cowintracker/in/MainActivity.java
@@ -47,7 +47,6 @@ public class MainActivity extends AppCompatActivity {
         cName.setOnClickListener(v -> {
             finish();
             startActivity(new Intent(MainActivity.this, CountryActivity.class));
-
         });
 
 
